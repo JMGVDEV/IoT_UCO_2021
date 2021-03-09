cc2486995b72bc4688fff3a29f2e0d29c6590507
Maknugss <cristian.566.ra@hotmail.com>
2021-03-09 00:12:13 -0500

    Se adjunta el codigo para el modulo ESP8266 y se hacen los cambios en timeZoneClock para integrarlo al modulo ESP8266

--- Practica_1/Practica2/TimeZoneClock.py
+++ Practica_1/Practica2/TimeZoneClock.py
@@ -1,13 +1,15 @@
-import paho.mqtt.client as mqtt
+import json
 import time
 import urllib.request
-import json
+import sys
+import paho.mqtt.client as mqtt
+import dateutil.parser as parser
 
 #Se conecta y suscribe al topico input
 def  on_connect(client, userdata, flags, rc):
         print("El cliente esta conectado")
         client.publish("alive", "Conexión exitosa")
-        client.subscribe("input/#")
+        client.subscribe("inputservicio/#")
 
 #Recibe el mensaje al topico suscrito e imprime (mensaje, topico)
 def on_message(client, userdata, message):   
@@ -24,19 +26,23 @@ def enviar_hora(localidad):
         client.publish("StatusRequest", "OK")        
         data = json.loads(response.read())
         client.publish("JsonStatus", str(data))
-        client.publish("output", data['datetime'])
-        print (data['datetime'])
-        
-    except: client.publish("StatusRequest", "Error 404")
-    
+        client.publish("outputservicio", formato_hora(data['datetime']))          
+    except: client.publish("StatusRequest", str(sys.exc_info()[0]))
 
-    
-
+#Cambia el formato que viene del Json al formato (Jueves, 18 de Febrero de 2021 -- 17:00)    
+def formato_hora(stringHora):
+        horaJSON = parser.parse(stringHora)
+        diaDiccionario = {"Monday": "lunes", "thuesday": "Martes", "Wednesday": "Miércoles", "thursday" : "Jueves", "Friday": "Viernes", "Saturday": "Sábado", "Sunday": "Domingo"}
+        mesDiccionario = {"January": "Enero", "February": "Febrero","March": "Marzo", "April": "Abril", "May": "Mayo", "June": "Junio", "July": "Julio", "August": "Agosto","September": "Septiembre", "October": "Octubre", "November": "Noviembre", "December": "Diciembre"}     
+        dia = diaDiccionario[horaJSON.strftime("%A")]
+        mes = mesDiccionario[horaJSON.strftime("%B")]
+        nuevoFormatoHora = dia + ", " + horaJSON.strftime("%d") + " de " + mes + horaJSON.strftime(" de %Y -- %H:%M") 
+        return(nuevoFormatoHora)
 
 #Parametros de conexión
-servidormqtt = "driver.cloudmqtt.com"
-usuario ="aspbcaly"
-password = "HlxL2hvTmTxC"
+servidormqtt = "****"
+usuario ="****"
+password = "*****"
 port = 18815 
 
 #Conexión al cliente de cloudMQTT
